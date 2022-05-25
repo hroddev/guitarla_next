cb21898096fd3b53224af5c5aa6a9e8fd8eb4a7f
Hector Rodriguez <hector@hrod.dev>
2022-05-25 08:56:58 -0500

    add content and css to Nosotros page

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,11 +1,24 @@
 import Layout from '../components/Layout'
+import Image from 'next/image'
+import styles from '../styles/Nosotros.module.css'
 
 const Nosotros = () => {
     return (
         <Layout
             pagina="Nosotros"
         >
-            <h1>Desde Nosotros</h1>
+            <main className='contenedor'>
+                <h2 className='heading'>Nosotros</h2>
+                <div className={styles.contenido}>
+                    <Image layout="responsive" width={600} height={450} src="/img/nosotros.jpg" alt="Imagen sobre nosotros"/>
+                    <div>
+                        <p>Ullamco anim do adipisicing cillum. Deserunt nisi elit cupidatat nulla pariatur in aliquip dolore laborum Lorem est labore ullamco. Excepteur eu dolor quis sint in. Cupidatat sint irure esse officia ut quis laboris. Consectetur est elit excepteur mollit nisi commodo sunt. Et tempor Lorem tempor nisi sit adipisicing.
+                        </p>
+                        <p>Ullamco anim do adipisicing cillum. Deserunt nisi elit cupidatat nulla pariatur in aliquip dolore laborum Lorem est labore ullamco. Excepteur eu dolor quis sint in. Cupidatat sint irure esse officia ut quis laboris. Consectetur est elit excepteur mollit nisi commodo sunt. Et tempor Lorem tempor nisi sit adipisicing.
+                        </p>
+                    </div>
+                </div>
+            </main>
 
         </Layout>
     )
